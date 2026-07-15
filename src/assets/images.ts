4dba610978411eabf10b0262566e5e69496d9f4b
Alexs1313 <o.salata@o-salata.local>
2026-07-15 13:10:05 +0300

    upd

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,42 +1,42 @@
 const images: Record<string, number> = {
   // Splash
-  LoaderBackground: require('./hippo-hub-loader-bg.png'),
-  LoaderIcon: require('./hippo-hub-loader-icon.png'),
+  LoaderBackground: require('./room-hoob-loader-bg.png'),
+  LoaderIcon: require('./room-hoob-loader-icon.png'),
 
   // Onboarding
-  Onboard1: require('./hippo-hub-onboard-1.png'),
-  Onboard2: require('./hippo-hub-onboard-2.png'),
-  Onboard3: require('./hippo-hub-onboard-3.png'),
-  Onboard4: require('./hippo-hub-onboard-4.png'),
-  Onboard5: require('./hippo-hub-onboard-5.png'),
+  Onboard1: require('./room-hoob-onboard-1.png'),
+  Onboard2: require('./room-hoob-onboard-2.png'),
+  Onboard3: require('./room-hoob-onboard-3.png'),
+  Onboard4: require('./room-hoob-onboard-4.png'),
+  Onboard5: require('./room-hoob-onboard-5.png'),
 
   // Tab bar icons
-  TabRequests: require('./hippo-hub-tab-requests.png'),
-  TabClimate: require('./hippo-hub-tab-climate.png'),
-  TabDine: require('./hippo-hub-tab-dine.png'),
-  TabParking: require('./hippo-hub-tab-parking.png'),
-  TabConcierge: require('./hippo-hub-tab-concierge.png'),
+  TabRequests: require('./room-hoob-tab-requests.png'),
+  TabClimate: require('./room-hoob-tab-climate.png'),
+  TabDine: require('./room-hoob-tab-dine.png'),
+  TabParking: require('./room-hoob-tab-parking.png'),
+  TabConcierge: require('./room-hoob-tab-concierge.png'),
 
   // Menu — Signatures
-  GrilledBeefTenderloin: require('./hippo-hub-grilled-beef-tenderloin.png'),
-  HerbRoastedSalmon: require('./hippo-hub-herb-roasted-salmon.png'),
-  TruffleMushroomRisotto: require('./hippo-hub-truffle-mushroom-risotto.png'),
-  ChickenSupreme: require('./hippo-hub-chicken-supreme.png'),
-  LambRackDeluxe: require('./hippo-hub-lamb-rack-deluxe.png'),
+  GrilledBeefTenderloin: require('./room-hoob-grilled-beef-tenderloin.png'),
+  HerbRoastedSalmon: require('./room-hoob-herb-roasted-salmon.png'),
+  TruffleMushroomRisotto: require('./room-hoob-truffle-mushroom-risotto.png'),
+  ChickenSupreme: require('./room-hoob-chicken-supreme.png'),
+  LambRackDeluxe: require('./room-hoob-lamb-rack-deluxe.png'),
 
   // Menu — Fresh & Light
-  CaesarSalad: require('./hippo-hub-caesar-salad.png'),
-  MediterraneanSalad: require('./hippo-hub-mediterranean-salad.png'),
-  AvocadoToastDeluxe: require('./hippo-hub-avocado-toast-deluxe.png'),
-  GrilledVegetableWrap: require('./hippo-hub-grilled-vegetable-wrap.png'),
-  BerryYogurtBowl: require('./hippo-hub-berry-yogurt-bowl.png'),
+  CaesarSalad: require('./room-hoob-caesar-salad.png'),
+  MediterraneanSalad: require('./room-hoob-mediterranean-salad.png'),
+  AvocadoToastDeluxe: require('./room-hoob-avocado-toast-deluxe.png'),
+  GrilledVegetableWrap: require('./room-hoob-grilled-vegetable-wrap.png'),
+  BerryYogurtBowl: require('./room-hoob-berry-yogurt-bowl.png'),
 
   // Menu — Desserts
-  ChocolateLavaCake: require('./hippo-hub-chocolate-lava-cake.png'),
-  NewYorkCheesecake: require('./hippo-hub-new-york-cheesecake.png'),
-  TiramisuClassic: require('./hippo-hub-tiramisu-classic.png'),
-  FreshFruitPlatter: require('./hippo-hub-fresh-fruit-platter.png'),
-  VanillaCremeBrulee: require('./hippo-hub-vanilla-creme-brulee.png'),
+  ChocolateLavaCake: require('./room-hoob-chocolate-lava-cake.png'),
+  NewYorkCheesecake: require('./room-hoob-new-york-cheesecake.png'),
+  TiramisuClassic: require('./room-hoob-tiramisu-classic.png'),
+  FreshFruitPlatter: require('./room-hoob-fresh-fruit-platter.png'),
+  VanillaCremeBrulee: require('./room-hoob-vanilla-creme-brulee.png'),
 };
 
 export const getImage = (key: string): number => images[key];
